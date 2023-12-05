e2edffa3e09705792aaac50e9fa28955313acac7
Tony Jimenez <84590027+gertony@users.noreply.github.com>
2023-12-04 21:08:49 -0500

    Create Empleado.java

---
+++ FASE 3/SESION9/EJERCICIOS/1/Empleado.java
@@ -0,0 +1,38 @@
+package com.mycompany.empleadosapp;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class Empleado {
+    private int numero;
+    private String nombre;
+    private double sueldo;
+
+    public Empleado(int numero, String nombre, double sueldo) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Número: " + numero + ", Nombre: " + nombre + ", Sueldo: " + sueldo;
+    }
+}
