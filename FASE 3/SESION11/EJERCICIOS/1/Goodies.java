05864d9f9a106ade9f7240f7af07b506b7437725
Tony Jimenez <84590027+gertony@users.noreply.github.com>
2023-12-05 00:48:17 -0500

    Create Goodies.java

---
+++ FASE 3/SESION11/EJERCICIOS/1/Goodies.java
@@ -0,0 +1,12 @@
+class Goodies {
+ private String name;
+ private double price;
+ public Goodies(String name, double price) {
+ this.name = name;
+ this.price = price;
+ }
+ public double getPrice() {
+ return price;
+ }
+ public String getName(){
+ return nam
